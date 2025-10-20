08344eafc2adc96b975f22a0edb55a90b9fabf7c
Diptipper <diptipper@gmail.com>
2025-10-20 22:29:51 +0900

    Add 3.1

---
+++ data/J31.js
@@ -0,0 +1,202 @@
+window.J31=`
+笑う わらう to laugh, to smile
+王 おう king
+王子 おうじ prince
+女王 じょおう queen
+部 ぶ department, division
+学部 がくぶ school department
+部下 ぶか subordinate
+部分 ぶぶん part, portion
+部長 ぶちょう business director, senior manager
+部屋 へや room
+彼 かれ he
+彼ら かれら they
+彼女 かのじょ she
+内 うち inside
+国内 こくない domestic, within country
+以内 いない within, less than
+内部 ないぶ inside, interior
+実は じつは as a matter of fact
+実家 じっか one's parents' home
+実力 じつりょく real ability, efficiency
+実習 じっしゅう hands-on or on-the-job training
+当たる あたる to be correct
+当時 とうじ at that time, in those days
+本当 ほんとう really, true
+本当に ほんとうに really, honestly
+日当たり ひあたり exposure to sun
+当たり前 あたりまえ obvious, natural, common
+戦う たたかう to make war
+戦い たたかい battle
+大戦 たいせん great war
+亡くなる なくなる to die, to pass away
+忙しい いそがしい busy
+性 せい gender
+女性 じょせい woman, female
+男性 だんせい man, male
+性別 せいべつ gender
+対 つい pair
+対する たいする to be facing
+関する かんする to concern
+関心 かんしん concern, interest
+無関心 むかんしん apathic, indifferent
+感じる かんじる to feel, to sense
+感動 かんどう being moved emotionally
+感心 かんしん admiration, being impressed
+定まる さだまる to become settled
+定員 ていいん max capacity
+一定 いってい fixed, constant
+定食 ていしょく pre-decided meal set
+定休日 ていきゅうび fixed holiday
+不安定 ふあんてい unstable, insecure
+合う あう to match, to be correct
+合わせる あわせる to combine, to unite
+場合 ばあい instance, occasion, situation
+試合 しあい game
+合図 あいず signal, cue
+集合 しゅうごう gather, to assemble
+見合い みあい formal marriage interview
+間に合う まにあう to make it in time
+口に合う くちにあう to be palatable
+知り合う しりあう to get to know someone
+話し合い はなしあい discussion
+話し合う はなしあう to discuss
+待ち合わせ まちあわせ appointment
+問い合わせ といあわせ inquiry
+待ち合わせる まちあわせる to rendezvous, to arrange to meet
+問い合わせる といあわせる to inquire
+政 まつりごと rule
+取る とる to take, to pick up
+ちり取り ちりとり dustpan
+聞き取り ききとり listening comprehension
+年を取る としをとる to get old
+取り出す とりだす to take out, to pick out
+聞き取る ききとる to catch a person's words
+所 ところ place
+近所 きんじょ neighborhood
+台所 だいどころ kitchen
+場所 ばしょ place, location
+長所 ちょうしょ someone's strong point, advantage
+住所 じゅうしょ address
+現れる あらわれる to appear, to become visible
+現場 げんば location of an event
+現代 げんだい nowadays, modern times
+現実 げんじつ reality
+現金 げんきん cash
+現代人 げんだいじん modern day person
+現実的 げんじつてき realistic
+現代的 げんだいてき modern
+工事現場 こうじげんば construction site
+最も もっとも most, extremely
+最中 さいちゅう in the middle of, in the height of
+最後 さいご last
+最大 さいだい largest
+最小 さいしょう smallest
+最多 さいた most
+最少 さいしょう least
+最悪 さいあく worst
+最新 さいしん newest
+最近 さいきん recently
+最高 さいこう highest, best
+化する かする to change into
+文化 ぶんか culture, civilization
+悪化 あっか deterioration, to worsen
+少子化 しょうしか declining birth rate
+民 たみ people
+国民 こくみん people of a country
+相 あい together
+相手 あいて companion, partner
+相当 そうとう considerable, substantial
+法 ほう law
+文法 ぶんぽう grammar
+方法 ほうほう method, technique
+法学 ほうがく the study of law
+全て すべて everything
+全く まったく really
+全体 ぜんたい whole, entirety
+全部 ぜんぶ all
+安全 あんぜん safely
+全部で ぜんぶで in total
+全体的 ぜんたいてき overall
+全国的 ぜんこくてき national
+安全運転 あんぜんうんてん safe driving
+情 じょう feelings
+友情 ゆうじょう friendship, camaraderie
+感情 かんじょう emotion, feelings
+感情的 かんじょうてき emotional
+向こう むこう on the other side
+向かう むかう to go towards
+方向 ほうこう direction, orientation
+平ら たいら flatness
+平日 へいじつ weekday, non-holiday
+平気 へいき calmness
+成る なる to become
+成田 なりた Narita (city near Tokyo)
+成長 せいちょう growth, to grow up
+成人の日 せいじんのひ coming of age day
+経つ たつ for time to pass, to elapse
+信じる しんじる to believe, to trust
+自信 じしん self-confidence
+送信 そうしん transmission, to send
+信用 しんよう trust, confidence
+面 めん face
+正面 しょうめん front, facade
+画面 がめん screen
+面白い おもしろい interesting
+真面目 まじめ serious, diligent
+不真面目 ふまじめ frivolous, insincere
+込む こむ to be crowded
+人込み ひとごみ crowd of people
+連れる つれる to take or lead someone
+連れて来る つれてくる to bring someone along
+連休 れんきゅう extended holiday
+連れてくる つれてくる to bring someone somewhere
+連れて行く つれていく to take someone somewhere
+原 はら field
+原料 げんりょう raw materials
+顏 かお face
+顏色 かおいろ complexion
+笑顏 えがお smiling face
+機 はた loom
+機会 きかい chance, opportunity
+欠ける かける to be chipped
+次に つぎに next
+次の つぎの next
+次女 じじょ second-oldest daughter
+次男 じなん second-oldest son
+お米 おこめ uncooked rice
+数える かぞえる to count
+数学 すうがく mathematics
+数日 すうじつ several days
+数年 すうねん several years
+少数 しょうすう small amount, minority
+数か月 すうかげつ several months
+美しい うつくしい beautiful
+回 かい times
+回す まわす to turn, to revolve
+数回 すうかい several times
+何回 なんかい how many times?
+回答 かいとう answer, form entry
+何回も なんかいも many times
+回り道 まわりみち detour, diversion
+表 おもて surface, front-side
+表す あらわす to express, to represent
+表れる あらわれる to become apparent
+表情 ひょうじょう facial expression
+表面 ひょうめん surface, face, exterior
+発表 はっぴょう announcement
+表現 ひょうげん presentation, to express
+無表情 むひょうじょう emotionless
+声 こえ voice
+大声 おおごえ loud voice
+報じる ほうじる to inform
+情報 じょうほう information, news
+要するに ようするに to put it simply
+重要 じゅうよう momentous, principal
+変 へん strange
+変わる かわる to change
+変える かえる to change, to transform
+大変 たいへん terribly
+変化 へんか change, alteration
+相変わらず あいかわらず as always
+`;
