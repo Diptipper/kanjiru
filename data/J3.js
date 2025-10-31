eb9764c91cf1214340c590537ec9fa84ea1ef114
Diptipper <diptipper@gmail.com>
2025-10-31 18:17:22 +0900

    add 3.2

---
+++ data/J3.js
@@ -0,0 +1,402 @@
+window.J3=`
+笑う わらう to laugh, to smile
+王 おう king
+王子 おうじ prince
+女王 じょおう queen
+部 ぶ department, division
+学部 がくぶ school department
+部下 ぶか subordinate
+部分 ぶぶん part, portion
+部長 ぶちょう business director, senior manager
+部屋 へや room
+彼 かれ he
+彼ら かれら they
+彼女 かのじょ she
+内 うち inside
+国内 こくない domestic, within country
+以内 いない within, less than
+内部 ないぶ inside, interior
+実は じつは as a matter of fact
+実家 じっか one's parents' home
+実力 じつりょく real ability, efficiency
+実習 じっしゅう hands-on or on-the-job training
+当たる あたる to be correct
+当時 とうじ at that time, in those days
+本当 ほんとう really, true
+本当に ほんとうに really, honestly
+日当たり ひあたり exposure to sun
+当たり前 あたりまえ obvious, natural, common
+戦う たたかう to make war
+戦い たたかい battle
+大戦 たいせん great war
+亡くなる なくなる to die, to pass away
+忙しい いそがしい busy
+性 せい gender
+女性 じょせい woman, female
+男性 だんせい man, male
+性別 せいべつ gender
+対 つい pair
+対する たいする to be facing
+関する かんする to concern
+関心 かんしん concern, interest
+無関心 むかんしん apathic, indifferent
+感じる かんじる to feel, to sense
+感動 かんどう being moved emotionally
+感心 かんしん admiration, being impressed
+定まる さだまる to become settled
+定員 ていいん max capacity
+一定 いってい fixed, constant
+定食 ていしょく pre-decided meal set
+定休日 ていきゅうび fixed holiday
+不安定 ふあんてい unstable, insecure
+合う あう to match, to be correct
+合わせる あわせる to combine, to unite
+場合 ばあい instance, occasion, situation
+試合 しあい game
+合図 あいず signal, cue
+集合 しゅうごう gather, to assemble
+見合い みあい formal marriage interview
+間に合う まにあう to make it in time
+口に合う くちにあう to be palatable
+知り合う しりあう to get to know someone
+話し合い はなしあい discussion
+話し合う はなしあう to discuss
+待ち合わせ まちあわせ appointment
+問い合わせ といあわせ inquiry
+待ち合わせる まちあわせる to rendezvous, to arrange to meet
+問い合わせる といあわせる to inquire
+政 まつりごと rule
+取る とる to take, to pick up
+ちり取り ちりとり dustpan
+聞き取り ききとり listening comprehension
+年を取る としをとる to get old
+取り出す とりだす to take out, to pick out
+聞き取る ききとる to catch a person's words
+所 ところ place
+近所 きんじょ neighborhood
+台所 だいどころ kitchen
+場所 ばしょ place, location
+長所 ちょうしょ someone's strong point, advantage
+住所 じゅうしょ address
+現れる あらわれる to appear, to become visible
+現場 げんば location of an event
+現代 げんだい nowadays, modern times
+現実 げんじつ reality
+現金 げんきん cash
+現代人 げんだいじん modern day person
+現実的 げんじつてき realistic
+現代的 げんだいてき modern
+工事現場 こうじげんば construction site
+最も もっとも most, extremely
+最中 さいちゅう in the middle of, in the height of
+最後 さいご last
+最大 さいだい largest
+最小 さいしょう smallest
+最多 さいた most
+最少 さいしょう least
+最悪 さいあく worst
+最新 さいしん newest
+最近 さいきん recently
+最高 さいこう highest, best
+化する かする to change into
+文化 ぶんか culture, civilization
+悪化 あっか deterioration, to worsen
+少子化 しょうしか declining birth rate
+民 たみ people
+国民 こくみん people of a country
+相 あい together
+相手 あいて companion, partner
+相当 そうとう considerable, substantial
+法 ほう law
+文法 ぶんぽう grammar
+方法 ほうほう method, technique
+法学 ほうがく the study of law
+全て すべて everything
+全く まったく really
+全体 ぜんたい whole, entirety
+全部 ぜんぶ all
+安全 あんぜん safely
+全部で ぜんぶで in total
+全体的 ぜんたいてき overall
+全国的 ぜんこくてき national
+安全運転 あんぜんうんてん safe driving
+情 じょう feelings
+友情 ゆうじょう friendship, camaraderie
+感情 かんじょう emotion, feelings
+感情的 かんじょうてき emotional
+向こう むこう on the other side
+向かう むかう to go towards
+方向 ほうこう direction, orientation
+平ら たいら flatness
+平日 へいじつ weekday, non-holiday
+平気 へいき calmness
+成る なる to become
+成田 なりた Narita (city near Tokyo)
+成長 せいちょう growth, to grow up
+成人の日 せいじんのひ coming of age day
+経つ たつ for time to pass, to elapse
+信じる しんじる to believe, to trust
+自信 じしん self-confidence
+送信 そうしん transmission, to send
+信用 しんよう trust, confidence
+面 めん face
+正面 しょうめん front, facade
+画面 がめん screen
+面白い おもしろい interesting
+真面目 まじめ serious, diligent
+不真面目 ふまじめ frivolous, insincere
+込む こむ to be crowded
+人込み ひとごみ crowd of people
+連れる つれる to take or lead someone
+連れて来る つれてくる to bring someone along
+連休 れんきゅう extended holiday
+連れてくる つれてくる to bring someone somewhere
+連れて行く つれていく to take someone somewhere
+原 はら field
+原料 げんりょう raw materials
+顏 かお face
+顏色 かおいろ complexion
+笑顏 えがお smiling face
+機 はた loom
+機会 きかい chance, opportunity
+欠ける かける to be chipped
+次に つぎに next
+次の つぎの next
+次女 じじょ second-oldest daughter
+次男 じなん second-oldest son
+お米 おこめ uncooked rice
+数える かぞえる to count
+数学 すうがく mathematics
+数日 すうじつ several days
+数年 すうねん several years
+少数 しょうすう small amount, minority
+数か月 すうかげつ several months
+美しい うつくしい beautiful
+回 かい times
+回す まわす to turn, to revolve
+数回 すうかい several times
+何回 なんかい how many times?
+回答 かいとう answer, form entry
+何回も なんかいも many times
+回り道 まわりみち detour, diversion
+表 おもて surface, front-side
+表す あらわす to express, to represent
+表れる あらわれる to become apparent
+表情 ひょうじょう facial expression
+表面 ひょうめん surface, face, exterior
+発表 はっぴょう announcement
+表現 ひょうげん presentation, to express
+無表情 むひょうじょう emotionless
+声 こえ voice
+大声 おおごえ loud voice
+報じる ほうじる to inform
+情報 じょうほう information, news
+要するに ようするに to put it simply
+重要 じゅうよう momentous, principal
+変 へん strange
+変わる かわる to change
+変える かえる to change, to transform
+大変 たいへん terribly
+変化 へんか change, alteration
+相変わらず あいかわらず as always
+お礼 おれい gratitude
+神 かみ god
+神社 じんじゃ shrine
+記 き account
+記事 きじ article, news story
+日記 にっき diary, journal
+記入 きにゅう form entry, to fill out a form
+和 わ sum
+和食 わしょく Japanese cuisine
+和室 わしつ Japanese-style room
+平和 へいわ peaceful
+引く ひく To pull
+引き出し ひきだし drawer
+引き分け ひきわけ competition draw, tie game
+引き出す ひきだす to pull out, to withdraw
+治る なおる to get well, to recover
+治す なおす to cure, to heal
+政治 せいじ politics
+政治家 せいじか politician
+決して けっして never, not in the least
+決まる きまる to be decided
+決める きめる to decide
+太い ふとい fat, thick
+太る ふとる to gain weight
+太め ふとめ thicker, plump
+受かる うかる to be accepted to, to pass
+受ける うける to get, to receive
+受信 じゅしん reception, to receive
+受け取る うけとる to receive, to take
+受信者 じゅしんしゃ recipient
+解く とく to solve, to undo
+正解 せいかい correct answer
+理解 りかい comprehension
+市 いち market
+市 し city
+市立 しりつ municipal institution
+市民 しみん citizen
+期 き period
+学期 がっき school term, semester
+期間 きかん period of time, interval
+前期 ぜんき first school semester
+後期 こうき second school semester
+同期 どうき one's contemporary
+長期 ちょうき long-term
+期待 きたい expectation, to hope
+様 よう appearing
+様子 ようす appearance, condition
+活ける いける to arrange
+生活 せいかつ life
+活動 かつどう activity
+頭 あたま head
+頭にくる あたまにくる to get angry
+頭がいい あたまがいい intelligent
+組 くみ set, group
+組む くむ to put together
+組み立てる くみたてる to assemble
+指 ゆび finger, toe
+指定 してい designate, reserve
+目指す めざす to aim at, to head for
+説 せつ theory
+小説 しょうせつ novel
+説明 せつめい explanation
+能 のう talent
+能力 のうりょく ability, faculty
+可能性 かのうせい potentiality, likelihood
+不可能 ふかのう impossible
+葉 は leaf, blade of grass
+言葉 ことば word, language
+秋葉原 あきはばら Akihabara (area of Tokyo)
+流れ ながれ stream
+流す ながす to drain
+流れる ながれる to stream
+一流 いちりゅう top-notch, leading
+流行 りゅうこう fashion, be in fashion
+流し台 ながしだい sink
+流行り はやり fad, craze
+流行る はやる to be in fashion, to be popular
+流行語 りゅうこうご buzzword, popular phrase
+然も さも really
+全然 ぜんぜん not at all
+当然 とうぜん natural
+不自然 ふしぜん unnatural, artificial
+初め はじめ beginning, start
+初めて はじめて for the first time
+最初 さいしょ first, beginning
+在る ある to be
+現在 げんざい now
+調べる しらべる to examine, to investigate
+調子 ちょうし tone, mood, condition
+調味料 ちょうみりょう seasoning, flavoring
+議 ぎ discussion
+会議 かいぎ meeting, conference
+会議室 かいぎしつ meeting room
+不思議 ふしぎ wonderful, mysterious
+直す なおす to cure, to fix
+正直 しょうじき honest, candid
+見直す みなおす to review, to re-examine
+言い直す いいなおす to restate
+正直者 しょうじきもの honest person
+夫 おっと husband
+選ぶ えらぶ to choose, to pick
+選手 せんしゅ athlete, player
+権 けん authority, power
+利 り advantage
+利用 りよう use, usage
+利口 りこう clever, intelligent
+制 せい system
+制作 せいさく production (film or book)
+続く つづく to continue
+続ける つづける to continue, to keep on
+手続き てつづき procedure, process
+石 いし stone
+進む すすむ to proceed, to move forward
+進学 しんがく progress to a higher-level school
+進歩 しんぽ progress, to improve
+伝わる つたわる to be communicated
+伝える つたえる to communicate, to report
+手伝う てつだう to help
+加える くわえる to add, to sum up
+助かる たすかる to be saved, to be rescued
+助ける たすける to save, to rescue
+点 てん spot, point
+終点 しゅうてん last stop, final destination
+欠点 けってん flaw, weakness
+産む うむ to give birth
+産業 さんぎょう industry
+お土産 おみやげ souvenir
+務める つとめる to be employed at
+事務 じむ office work
+公務員 こうむいん civil servant
+事務所 じむしょ office
+件 くだん the aforementioned
+件名 けんめい subject, title
+命 いのち life, lifetime
+１１０番 ひゃくとおばん 110 (the Japanese emergency number)
+何番 なんばん what number?
+一番 いちばん first, best
+二番 にばん second
+三番 さんばん third
+四番 よんばん fourth
+五番 ごばん fifth
+六番 ろくばん sixth
+七番 ななばん seventh
+八番 はちばん eighth
+九番 きゅうばん ninth
+十番 じゅうばん tenth
+番組 ばんぐみ TV program
+落ちる おちる to fall
+落とす おとす to drop
+落ち着く おちつく to compose oneself
+落ち込む おちこむ to get depressed
+付く つく to be attached
+付ける つける to attach
+日付 ひづけ date
+受付 うけつけ reception desk
+気付く きづく to realize, to notice
+付き合い つきあい socializing, fellowship
+見付かる みつかる to be found
+付き合う つきあう to be in a relationship with
+見付ける みつける to discover, to find fault
+気を付ける きをつける to be careful
+受け付ける うけつける to receive inquiries or applications
+得 とく bargain, to get a bargain
+得る える to procure, to obtain
+得意 とくい one's forte
+好み このみ liking, one's taste
+好む このむ to favor, to prefer
+好き すき like, fondness
+友好 ゆうこう friendship
+友好国 ゆうこうこく friendly nation, ally
+違う ちがう to differ from, to be wrong
+間違い まちがい mistake, accident
+間違う まちがう to make a mistake
+間違える まちがえる to make a mistake
+殺す ころす to kill
+自殺 じさつ suicide
+置く おく to put, to place
+物置 ものおき storage room
+返す かえす to return something
+ひっくり返る ひっくりかえる to be flipped over
+ひっくり返す ひっくりかえす to turn over
+返事 へんじ reply, answer
+返信 へんしん reply, to reply
+言い返す いいかえす to answer back
+聞き返す ききかえす to ask or listen again
+論じる ろんじる to discuss
+論文 ろんぶん thesis
+示す しめす to tell, to demonstrate
+指示 しじ direction, instruction
+際 さい on the occasion of
+国際 こくさい international
+国際化 こくさいか globalization
+歳 さい years-old
+何歳 なんさい how old?
+反 たん measure of fabric or land
+反対 はんたい opposition, objection
+スピード違反 スピードいはん illegal speeding in vehicle
+形 かたち shape, form
+光る ひかる to shine
+日光 にっこう Nikko (city)
+`;
---
+++ data/J32.js
@@ -0,0 +1,202 @@
+window.J31=`
+お礼 おれい gratitude
+神 かみ god
+神社 じんじゃ shrine
+記 き account
+記事 きじ article, news story
+日記 にっき diary, journal
+記入 きにゅう form entry, to fill out a form
+和 わ sum
+和食 わしょく Japanese cuisine
+和室 わしつ Japanese-style room
+平和 へいわ peaceful
+引く ひく To pull
+引き出し ひきだし drawer
+引き分け ひきわけ competition draw, tie game
+引き出す ひきだす to pull out, to withdraw
+治る なおる to get well, to recover
+治す なおす to cure, to heal
+政治 せいじ politics
+政治家 せいじか politician
+決して けっして never, not in the least
+決まる きまる to be decided
+決める きめる to decide
+太い ふとい fat, thick
+太る ふとる to gain weight
+太め ふとめ thicker, plump
+受かる うかる to be accepted to, to pass
+受ける うける to get, to receive
+受信 じゅしん reception, to receive
+受け取る うけとる to receive, to take
+受信者 じゅしんしゃ recipient
+解く とく to solve, to undo
+正解 せいかい correct answer
+理解 りかい comprehension
+市 いち market
+市 し city
+市立 しりつ municipal institution
+市民 しみん citizen
+期 き period
+学期 がっき school term, semester
+期間 きかん period of time, interval
+前期 ぜんき first school semester
+後期 こうき second school semester
+同期 どうき one's contemporary
+長期 ちょうき long-term
+期待 きたい expectation, to hope
+様 よう appearing
+様子 ようす appearance, condition
+活ける いける to arrange
+生活 せいかつ life
+活動 かつどう activity
+頭 あたま head
+頭にくる あたまにくる to get angry
+頭がいい あたまがいい intelligent
+組 くみ set, group
+組む くむ to put together
+組み立てる くみたてる to assemble
+指 ゆび finger, toe
+指定 してい designate, reserve
+目指す めざす to aim at, to head for
+説 せつ theory
+小説 しょうせつ novel
+説明 せつめい explanation
+能 のう talent
+能力 のうりょく ability, faculty
+可能性 かのうせい potentiality, likelihood
+不可能 ふかのう impossible
+葉 は leaf, blade of grass
+言葉 ことば word, language
+秋葉原 あきはばら Akihabara (area of Tokyo)
+流れ ながれ stream
+流す ながす to drain
+流れる ながれる to stream
+一流 いちりゅう top-notch, leading
+流行 りゅうこう fashion, be in fashion
+流し台 ながしだい sink
+流行り はやり fad, craze
+流行る はやる to be in fashion, to be popular
+流行語 りゅうこうご buzzword, popular phrase
+然も さも really
+全然 ぜんぜん not at all
+当然 とうぜん natural
+不自然 ふしぜん unnatural, artificial
+初め はじめ beginning, start
+初めて はじめて for the first time
+最初 さいしょ first, beginning
+在る ある to be
+現在 げんざい now
+調べる しらべる to examine, to investigate
+調子 ちょうし tone, mood, condition
+調味料 ちょうみりょう seasoning, flavoring
+議 ぎ discussion
+会議 かいぎ meeting, conference
+会議室 かいぎしつ meeting room
+不思議 ふしぎ wonderful, mysterious
+直す なおす to cure, to fix
+正直 しょうじき honest, candid
+見直す みなおす to review, to re-examine
+言い直す いいなおす to restate
+正直者 しょうじきもの honest person
+夫 おっと husband
+選ぶ えらぶ to choose, to pick
+選手 せんしゅ athlete, player
+権 けん authority, power
+利 り advantage
+利用 りよう use, usage
+利口 りこう clever, intelligent
+制 せい system
+制作 せいさく production (film or book)
+続く つづく to continue
+続ける つづける to continue, to keep on
+手続き てつづき procedure, process
+石 いし stone
+進む すすむ to proceed, to move forward
+進学 しんがく progress to a higher-level school
+進歩 しんぽ progress, to improve
+伝わる つたわる to be communicated
+伝える つたえる to communicate, to report
+手伝う てつだう to help
+加える くわえる to add, to sum up
+助かる たすかる to be saved, to be rescued
+助ける たすける to save, to rescue
+点 てん spot, point
+終点 しゅうてん last stop, final destination
+欠点 けってん flaw, weakness
+産む うむ to give birth
+産業 さんぎょう industry
+お土産 おみやげ souvenir
+務める つとめる to be employed at
+事務 じむ office work
+公務員 こうむいん civil servant
+事務所 じむしょ office
+件 くだん the aforementioned
+件名 けんめい subject, title
+命 いのち life, lifetime
+１１０番 ひゃくとおばん 110 (the Japanese emergency number)
+何番 なんばん what number?
+一番 いちばん first, best
+二番 にばん second
+三番 さんばん third
+四番 よんばん fourth
+五番 ごばん fifth
+六番 ろくばん sixth
+七番 ななばん seventh
+八番 はちばん eighth
+九番 きゅうばん ninth
+十番 じゅうばん tenth
+番組 ばんぐみ TV program
+落ちる おちる to fall
+落とす おとす to drop
+落ち着く おちつく to compose oneself
+落ち込む おちこむ to get depressed
+付く つく to be attached
+付ける つける to attach
+日付 ひづけ date
+受付 うけつけ reception desk
+気付く きづく to realize, to notice
+付き合い つきあい socializing, fellowship
+見付かる みつかる to be found
+付き合う つきあう to be in a relationship with
+見付ける みつける to discover, to find fault
+気を付ける きをつける to be careful
+受け付ける うけつける to receive inquiries or applications
+得 とく bargain, to get a bargain
+得る える to procure, to obtain
+得意 とくい one's forte
+好み このみ liking, one's taste
+好む このむ to favor, to prefer
+好き すき like, fondness
+友好 ゆうこう friendship
+友好国 ゆうこうこく friendly nation, ally
+違う ちがう to differ from, to be wrong
+間違い まちがい mistake, accident
+間違う まちがう to make a mistake
+間違える まちがえる to make a mistake
+殺す ころす to kill
+自殺 じさつ suicide
+置く おく to put, to place
+物置 ものおき storage room
+返す かえす to return something
+ひっくり返る ひっくりかえる to be flipped over
+ひっくり返す ひっくりかえす to turn over
+返事 へんじ reply, answer
+返信 へんしん reply, to reply
+言い返す いいかえす to answer back
+聞き返す ききかえす to ask or listen again
+論じる ろんじる to discuss
+論文 ろんぶん thesis
+示す しめす to tell, to demonstrate
+指示 しじ direction, instruction
+際 さい on the occasion of
+国際 こくさい international
+国際化 こくさいか globalization
+歳 さい years-old
+何歳 なんさい how old?
+反 たん measure of fabric or land
+反対 はんたい opposition, objection
+スピード違反 スピードいはん illegal speeding in vehicle
+形 かたち shape, form
+光る ひかる to shine
+日光 にっこう Nikko (city)
+`;
